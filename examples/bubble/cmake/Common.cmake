688201985f153a08421e2960ce1e9ac5c10bf319
kfields <kurtisfields@gmail.com>
2026-05-02 23:54:53 -0600

    cleanup examples

--- examples/bubble/cmake/Common.cmake
+++
@@ -1,8 +0,0 @@
-include_guard()
-
-include(${AUGR_CMAKE}/AugrCore.cmake)
-
-function(USES_COMMON THIS)
-USES_AUGR_CORE(${THIS})
-target_include_directories(${THIS} PRIVATE ${PROJECT_ROOT})
-endfunction()
--- examples/panner/app.h
+++ examples/panner/app.h
@@ -1,5 +1,8 @@
 #include <augite/app/app.h>
 
 class MyApp : public augr::App {
-  void Draw();
+    void Draw();
+    // Data members
+    bool show_demo_window = true;
+    bool show_another_window = false;
 };
